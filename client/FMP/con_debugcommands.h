475e87b0e867c68319b6855f62a58baffc258f3b
fatonythebest <fatonythebest@53c5ce50-8a71-11de-b74b-2df4680eaf45>
2010-06-14 18:08:37 +0000

    Added "give" debug console command.

--- client/FMP/con_debugcommands.h
+++ client/FMP/con_debugcommands.h
@@ -6,5 +6,8 @@ void ConCmdAddarmor(ConsoleCore *concore, const unsigned char numargs);
 void ConCmdGetarmor(ConsoleCore *concore, const unsigned char numargs);
 void ConCmdGethealth(ConsoleCore *concore, const unsigned char numargs);
 
+/// \brief Give weapon to player.
+void ConCmdGive(ConsoleCore *concore, const unsigned char numargs);
+
 /// \brief Hurts the player.
 void ConCmdHurtme(ConsoleCore *concore, const unsigned char numargs);
