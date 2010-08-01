aa837668147766296aa5af4217a08ab3aba05b6f
wnezros <wnezros@53c5ce50-8a71-11de-b74b-2df4680eaf45>
2010-08-01 09:19:31 +0000

    base Connect/Disconnect

--- branches/nextgen/server/main.h
+++ branches/nextgen/server/main.h
@@ -2,3 +2,5 @@
 
 #define FMP_VERSION "0.2n"
 #define MAX_PLAYERS 32
+
+#define TODO(data) printf("TODO: %s\n", data)
--- branches/nextgen/server/network/CNetworkMain.cpp
+++ branches/nextgen/server/network/CNetworkMain.cpp
@@ -5,6 +5,7 @@
 CNetwork::CNetwork()
 {
 	m_pNet = NULL;
+	m_pPlayerAddress = NULL; 
 	iMaxPlayers = 0;
 }
 
@@ -31,6 +32,13 @@ bool CNetwork::Load(const unsigned short iPort, const unsigned short iMaxPlayers
 
 bool CNetwork::Unload()
 {
+	if(m_pPlayerAddress) 
+	{
+		for(int i = 0; i < iMaxPlayers; i++)
+			if(m_pPlayerAddress[i].port != 0) this->HandleDisconnection(m_pPlayerAddress[i]);
+
+		delete [] m_pPlayerAddress;
+	}
 	if (m_pNet)
 	{
 		m_pNet->Shutdown(100,0);
@@ -44,6 +52,21 @@ bool CNetwork::IsReady()
 	return this->m_pNet != NULL;
 }
 
+short CNetwork::GetIdFromAddress(SystemAddress saAddr)
+{
+	for(int i = 0; i < iMaxPlayers; i++)
+		if(m_pPlayerAddress[i] == saAddr) return i;
+
+	return -1;
+}
+
+short CNetwork::GetFreeClientId()
+{
+	for(int i = 0; i < iMaxPlayers; i++)
+		if(m_pPlayerAddress[i].port == 0) return i;
+	return -1;
+}
+
 void CNetwork::Tick()
 {
 	static Packet * pPack;
@@ -53,7 +76,7 @@ void CNetwork::Tick()
 		{
 		case FMP_PACKET_SIGNATURE:
 			{
-				//this->HandlePacket(pack->data + 1, pack->length - 1, pack->systemAddress);
+				this->HandlePacket(pPack->data + 1, pPack->length - 1, pPack->systemAddress);
 			} break;
 		case ID_CONNECTION_REQUEST:
 			{
@@ -65,12 +88,74 @@ void CNetwork::Tick()
 			} break;
 		case ID_DISCONNECTION_NOTIFICATION:
 			{
-				//this->HandleClientDisconnection(pack->systemAddress);
+				this->HandleDisconnection(pPack->systemAddress);
 			} break;
 		case ID_CONNECTION_LOST:
 			{
-				//this->HandleClientDisconnection(pack->systemAddress);
+				this->HandleDisconnection(pPack->systemAddress);
 			} break;
 		}
+	}
+}
+
+void CNetwork::HandlePacket(const unsigned char * pData, const unsigned int dwLength, const SystemAddress saAddr)
+{
+	short sType = *(short *)(pData);
+	switch(sType)
+	{
+	case NetworkData::NetworkConnect:
+		{
+			this->HandleConnect((NetworkData::Connect *)(pData + 2), dwLength, saAddr); 
+		} break;
+	default:
+		{
+			Log::Warning("Unknown packet %d (Length: %d) from %s", sType, dwLength, saAddr.ToString());
+		} break;
+	}
+}
+
+void CNetwork::HandleConnect(const NetworkData::Connect * pData, const unsigned int dwLength, const SystemAddress saAddr)
+{
+	if(pData->version < FMP_NETWORK_VERSION)
+	{
+		this->SendClientConnect(-1, NetworkData::CR_OLDFMP, saAddr);
+		return;
+	}
+
+	short iID = this->GetIdFromAddress(saAddr);
+	if(iID > 0 && iID < iMaxPlayers)
+	{
+		this->SendClientConnect(iID, NetworkData::CR_OK, saAddr);
+		return;
+	}
+	iID = this->GetFreeClientId();
+	if(iID < 0 || iID >= iMaxPlayers)
+	{
+		this->SendClientConnect(iID, NetworkData::CR_NOFREESLOTS, saAddr);
+		return;
+	}
+
+	for(int i = 0; i < m_vBadNick.size(); i++)
+	{
+		if(wcscmp(m_vBadNick.at(i), pData->nick) == 0)
+		{
+			this->SendClientConnect(iID, NetworkData::CR_BANNED, saAddr);
+			return;
+		}
+	}
+
+	TODO("Nick check");
+	TODO("Send to script");
+
+	this->SendClientConnect(iID, NetworkData::CR_OK, saAddr);
+}
+
+void CNetwork::HandleDisconnection(const SystemAddress saAddr)
+{
+	short iID = GetIdFromAddress(saAddr);
+	if(iID > 0 && iID < iMaxPlayers)
+	{
+		this->SendClientDisconnect(iID);
+		memset(&m_pPlayerAddress[iID], 0, sizeof(SystemAddress));
 	}
 }
