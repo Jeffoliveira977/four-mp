4dbb4f8b605bbf462808421db79efbd877646af9
wnezros@gmail.com <wnezros@gmail.com@53c5ce50-8a71-11de-b74b-2df4680eaf45>
2011-07-22 15:54:57 +0000

    Code style patch

--- trunk/client/FMP/Hook/natives.cpp
+++ trunk/client/FMP/Hook/natives.cpp
@@ -5,45 +5,40 @@
 
 extern DWORD dwLoadOffset;
 
-int GetNativeAddressByHash(unsigned int a1)
+struct _SCRIPTPTRSTRUC{
+	unsigned int hash;
+	int addr;
+}*scriptptr;
+
+int GetNativeAddressByHash(unsigned int a1) // { VoLT }
 {
 	if(a1 == -1) return 0;
-	//Log::Debug("# GetNativeAddress: 0x%x , natives count: %d",a1,*(DWORD*)(0x18DF6EC+dwLoadOffset));
 
 	DWORD pointer1 = *(DWORD*)SCRIPT_POINTER_1;
-	DWORD pointer2 = *(DWORD*)SCRIPT_POINTER_2;
+	scriptptr = *(_SCRIPTPTRSTRUC**)SCRIPT_POINTER_2;
 
-	//Log::Debug("# pointers: [0x%x]:(%d),[0x%x]:(%d)",pointer1,pointer1,pointer2,pointer2);
-
-	unsigned int v2;
 	unsigned int v3;
 	unsigned int v4;
 	unsigned int hash_pointer;
-
-	if(!pointer1) 
-	{
-		Log::Debug(L"# not find pointer");
+	if(!pointer1){
+		Log::Debug("# not find pointer");
 		return 0;
 	}
-	// reset data
 	v3 = a1 % pointer1;
 	v4 = a1;
-	// first pointer
-	hash_pointer = *(DWORD *)(pointer2 + 8 * v3);
-	while(hash_pointer != a1)
-	{
+	hash_pointer = scriptptr[v3].hash;
+	while(hash_pointer != a1){
 		v4 = (v4 >> 1) + 1;
 		v3 = (v4 + v3) % pointer1;
-		hash_pointer = *(DWORD *)(pointer2 + (8 * v3));
-		//Log::Debug("# hash_pointer: 0x%x [%d]",hash_pointer,hash_pointer);
+		hash_pointer = scriptptr[v3].hash;
+		if(hash_pointer == 0){
+			Log::Debug("# hash_pointer null");
+			return 0;
+		}
 	}
-	//Log::Debug("# hash_pointer: 0x%x [%d]",hash_pointer,hash_pointer);
-	if(hash_pointer == 0)
-	{
-		Log::Debug(L"# hash_pointer null");
+	if(hash_pointer != 0)
+		return scriptptr[v3].addr;
+	// ересь по другому мозг не придумал
+	Log::Debug("# hash_pointer null");
 		return 0;
-	}
-	int addr = (int)(*(DWORD*)(pointer2 + (8 * v3) + 4));
-	//Log::Debug("# addr = 0x%x",addr);
-	return addr;
 }
